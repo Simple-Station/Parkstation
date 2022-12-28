70621c3a794c23e3d7c2f69b159cc3f41b12d9c8
DEATHB4DEFEAT <zachcaffee@outlook.com>
2022-12-28 00:03:26 -0800

    FLAMING PLASMAMEN

--- Content.Shared/SimpleStation14/Magic/Components/ClothingGrantComponentComponent.cs
+++ Content.Shared/SimpleStation14/Magic/Components/ClothingGrantComponentComponent.cs
@@ -3,8 +3,10 @@ namespace Content.Shared.SimpleStation14.Magic
     [RegisterComponent]
     public sealed class ClothingGrantComponentComponent : Component
     {
-        [DataField("component", required: true), ViewVariables(VVAccess.ReadWrite)]
-        public string Component = "";
+        [DataField("component"), ViewVariables(VVAccess.ReadWrite)]
+        public string? Component = null;
+        [DataField("tag"), ViewVariables(VVAccess.ReadWrite)]
+        public string? Tag = null;
 
         [ViewVariables(VVAccess.ReadWrite)]
         public bool IsActive = false;
