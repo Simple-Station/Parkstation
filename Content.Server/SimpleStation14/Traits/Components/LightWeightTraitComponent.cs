0cf29e8902ac8e00ed261c3d159a45fa39205079
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-01-13 19:56:22 -0800

    Change Voracious trait, add LightWeight trait, tweak trait costs, fix hunger being -1 roundstart

---
+++ Content.Server/SimpleStation14/Traits/Components/LightWeightTraitComponent.cs
@@ -0,0 +1,26 @@
+namespace Content.Shared.SimpleStation14.Traits
+{
+    [RegisterComponent]
+    public sealed class LightWeightTraitComponent : Component
+    {
+        /// <summary>
+        /// How much time to remove from climbing (in seconds)
+        /// </summary>
+        [DataField("negateTime"), ViewVariables(VVAccess.ReadWrite)]
+        public float? NegateTime;
+
+
+        // Actual speed names (after park change)
+        /// <summary>
+        /// MovementSpeedModifierComponent.BaseWalkSpeed
+        /// </summary>
+        [DataField("sprint"), ViewVariables(VVAccess.ReadOnly)]
+        public float Sprint = 1f;
+
+        /// <summary>
+        /// MovementSpeedModifierComponent.BaseSprintSpeed
+        /// </summary>
+        [DataField("walk"), ViewVariables(VVAccess.ReadOnly)]
+        public float Walk = 1.25f;
+    }
+}
