8ea61f178cacc2d681f9e4e3f64f6f416d6d6139
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-01-25 08:56:39 -0500

    Set up underwear to equip on round start by default, while still allowing override underwear in role files (check boxer.yml for a full list of overrides). Removed coder socks and bee socks, replaced them with undersock variants. Set a few roles to have custom underwear.

--- Content.Shared/Roles/StartingGearPrototype.cs
+++ Content.Shared/Roles/StartingGearPrototype.cs
@@ -22,6 +22,23 @@ namespace Content.Shared.Roles
         [DataField("duffelbag", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
         private string _duffelbag = string.Empty;
 
+        // Underwear
+
+        [DataField("underpants", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        private string _underpants = string.Empty;
+
+        [DataField("underpantsskirt", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        private string _underpantsskirt = string.Empty;
+
+        [DataField("undershirt", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        private string _undershirt = string.Empty;
+
+        [DataField("undershirtskirt", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        private string _undershirtskirt = string.Empty;
+
+        [DataField("undersocks", customTypeSerializer:typeof(PrototypeIdSerializer<EntityPrototype>))]
+        private string _undersocks = string.Empty;
+
         public IReadOnlyDictionary<string, string> Inhand => _inHand;
         /// <summary>
         /// hand index, item prototype
@@ -57,6 +74,40 @@ namespace Content.Shared.Roles
 
                     if (booleanthing == true) return "ClothingEyesGlasses";
                 }
+                // Commando no longer, underwear shall be worn.
+
+                if (slot == "socks" && string.IsNullOrEmpty(_undersocks))
+                    return "ClothingUnderSocks_norm";
+
+                // Handles equipping all crew with underwear without putting it in every file.
+                // Checks for skirt settings, if skirt = true, equip with panties and bra
+                // if skirt = false, equip with boxers and shirt.
+                // Using else caused weird issues I didn't feel like dealing with -Psp
+
+                if (slot == "underpants" && profile.Clothing != ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_underpants))
+                    return "ClothingUnderboxer_briefs";
+                if (slot == "underpants" && profile.Clothing == ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_underpantsskirt))
+                    return "ClothingUnderpanties";
+
+                if (slot == "undershirt" && profile.Clothing != ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_undershirt))
+                    return "ClothingUnderundershirt";
+                if (slot == "undershirt" && profile.Clothing == ClothingPreference.Jumpskirt && string.IsNullOrEmpty(_undershirtskirt))
+                    return "ClothingUnderbra";
+
+                // Handles custom underwear per role.
+
+                if (slot == "underpants" && profile.Clothing != ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_underpants) && _underpants != "empty")
+                    return _underpants;
+                if (slot == "underpants" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_underpantsskirt) && _underpantsskirt != "empty")
+                    return _underpantsskirt;
+
+                if (slot == "undershirt" && profile.Clothing != ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_undershirt) && _undershirt != "empty")
+                    return _undershirt;
+                if (slot == "undershirt" && profile.Clothing == ClothingPreference.Jumpskirt && !string.IsNullOrEmpty(_undershirtskirt) && _undershirtskirt != "empty")
+                    return _undershirtskirt;
+
+                if (slot == "socks" && !string.IsNullOrEmpty(_undersocks) && _undersocks != "empty")
+                    return _undersocks;
             }
 
             return _equipment.TryGetValue(slot, out var equipment) ? equipment : string.Empty;
