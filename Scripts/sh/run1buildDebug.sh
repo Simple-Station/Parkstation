50bd17c12a93e43062eda27cc83bb4963c1cdc50
Aria Moradi <aria.moradi007@gmail.com>
2023-01-30 16:20:56 +0330

    allow scripts to be run from root

--- Scripts/sh/run1buildDebug.sh
+++ Scripts/sh/run1buildDebug.sh
@@ -1,3 +1,9 @@
-cd ../../
+#!/bin/env sh
+
+# make sure running from root
+if [ "$(dirname $0)" != "." ]; then
+  cd "$(dirname $0)"
+fi
+cd ../..
+
 dotnet build -c Debug
-pause
--- Scripts/sh/run1buildRelease.sh
+++ Scripts/sh/run1buildRelease.sh
@@ -1,3 +1,11 @@
-cd ../../
+#!/bin/env sh
+
+# make sure to start from script dir
+if [ "$(dirname $0)" != "." ]; then
+  cd "$(dirname $0)"
+fi
+
+# make sure running from root
+cd ../..
+
 dotnet build -c Release
-pause
