62bc5db1514fda06a14ed090c0f15902d934a9d1
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-01-31 07:30:03 -0500

    Made DropOnSlip component functional, with a percent chance, made items in pockets have a chance to drop. Gave several clothing items DropOnSlipComponent with various chances. Still need to integrate clumsy.

--- Content.Shared/SimpleStation14/Slippery/DropOnSlipComponent.cs
+++ Content.Shared/SimpleStation14/Slippery/DropOnSlipComponent.cs
@@ -3,5 +3,8 @@ namespace Content.Shared.Slippery
     [RegisterComponent]
     public sealed class DropOnSlipComponent : Component
     {
+        [DataField("chance")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public int Chance = 20;
     }
 }
--- Content.Shared/Slippery/SlipperySystem.cs
+++ Content.Shared/Slippery/SlipperySystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Administration.Logs;
+using Content.Shared.Clothing.Components;
 using Content.Shared.Database;
 using Content.Shared.Inventory;
 using Content.Shared.StatusEffect;
@@ -10,6 +11,8 @@ using Robust.Shared.Containers;
 using Robust.Shared.GameStates;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
+using Robust.Shared.Random;
+using Content.Shared.Interaction.Components;
 
 namespace Content.Shared.Slippery
 {
@@ -23,6 +26,8 @@ namespace Content.Shared.Slippery
         [Dependency] private readonly SharedContainerSystem _container = default!;
         [Dependency] private readonly SharedPhysicsSystem _physics = default!;
         [Dependency] private readonly IEntityManager _entities = default!;
+        [Dependency] private readonly IRobustRandom _random = default!;
+
 
 
         public override void Initialize()
@@ -98,7 +103,23 @@ namespace Content.Shared.Slippery
             var invSystem = _entities.System<InventorySystem>();
             if (invSystem.TryGetSlots(other, out var slotDefinitions, inventoryComponent))
             {
-                invSystem.TryUnequip(other, "eyes", true, true, true, inventoryComponent);
+                foreach (var slot in slotDefinitions)
+                {
+                    invSystem.TryGetSlotEntity(other, slot.Name, out var item);
+                    _entities.TryGetComponent<ClothingComponent>(item, out var clothingComp);
+                    if (_entities.TryGetComponent<DropOnSlipComponent>(item, out var dropComp) && slot.Name != "pocket1" && slot.Name != "pocket2" && (_random.NextFloat(0, 100) < dropComp.Chance))
+                    {
+                        invSystem.TryUnequip(other, slot.Name, true, true, false, inventoryComponent);
+                    }
+                    // else if (_random.NextFloat(0, 100) < 5 && _entities.TryGetComponent<ClumsyComponent>(other, out var ____) && slot.Name != "jumpsuit")
+                    // {
+                    //     invSystem.TryUnequip(other, slot.Name, true, true, false, inventoryComponent);
+                    // }
+                    else if (_random.NextFloat(0, 100) < 10 && slot.Name == "pocket1" | slot.Name == "pocket2")
+                    {
+                        invSystem.TryUnequip(other, slot.Name, true, true, false, inventoryComponent);
+                    }
+                }
             }
 
             // Preventing from playing the slip sound when you are already knocked down.
