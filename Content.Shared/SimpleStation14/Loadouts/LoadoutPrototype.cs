a7140b2467ba07882eaa70bfe821a99f4cfe70dd
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-02-02 14:23:16 -0800

    Start Loadouts, read commit description - HumanoidProfileEditor Tab - Locale - LoadoutPrototype - 2 test prototypes, Prototypes/SimpleStation14/Loadouts/test.yml - LoadoutPreferenceSelector - TestLoadout

---
+++ Content.Shared/SimpleStation14/Loadouts/LoadoutPrototype.cs
@@ -0,0 +1,49 @@
+using Content.Shared.Whitelist;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.SimpleStation14.Loadouts
+{
+    /// <summary>
+    ///     Describes a loadout.
+    /// </summary>
+    [Prototype("loadout")]
+    public sealed class LoadoutPrototype : IPrototype
+    {
+        [ViewVariables]
+        [IdDataField]
+        public string ID { get; } = default!;
+
+        /// <summary>
+        ///     The name of this loadout.
+        /// </summary>
+        [DataField("name")]
+        public string Name { get; private set; } = "";
+
+        /// <summary>
+        ///     The description of this loadout.
+        /// </summary>
+        [DataField("description")]
+        public string? Description { get; private set; }
+
+        /// <summary>
+        ///     The point cost of this loadout.
+        /// </summary>
+        [DataField("cost")]
+        public int Cost = 0;
+
+        /// <summary>
+        ///     Don't apply this loadout to entities this whitelist IS NOT valid for.
+        /// </summary>
+        [DataField("whitelist")]
+        public EntityWhitelist? Whitelist;
+
+        /// <summary>
+        ///     Don't apply this loadout to entities this whitelist IS valid for. (hence, a blacklist)
+        /// </summary>
+        [DataField("blacklist")]
+        public EntityWhitelist? Blacklist;
+
+        [DataField("item")]
+        public string? Item;
+    }
+}
