9bcb2782e7a646cc39484a7f903fa84432a3bf4e
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-02-14 03:01:15 -0500

    Working on it

--- Content.Shared/SimpleStation14/Drone/HologramComponent.cs
+++ Content.Shared/SimpleStation14/Drone/HologramComponent.cs
@@ -2,5 +2,8 @@ namespace Content.Shared.SimpleStation14.Hologram
 {
     [RegisterComponent]
     public sealed class HologramComponent : Component
-    {}
+    {
+        [ViewVariables]
+        public EntityUid? CurProjector;
+    }
 }
