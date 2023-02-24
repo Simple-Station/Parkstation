fae4dcb161a9ef061e4e1072b620db0b7a5d9c03
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-02-24 18:42:07 -0500

    Lots more work towards holograms, probably too long without committing :P

---
+++ Content.Server/SimpleStation14/Holograms/Components/HoloDiskComponent.cs
@@ -0,0 +1,19 @@
+using static Content.Server.SimpleStation14.Hologram.HologramSystem;
+
+namespace Content.Server.SimpleStation14.Hologram;
+
+[RegisterComponent]
+public sealed class HologramDiskComponent : Component
+{
+    // [ViewVariables]
+    // public HoloDataEntry? HoloData = null;
+
+    // [DataField("active"), ViewVariables(VVAccess.ReadWrite)]
+    // public bool Active = true;
+
+    [ViewVariables]
+    public Mind.Mind? HoloData = null;
+
+    [DataField("active"), ViewVariables(VVAccess.ReadWrite)]
+    public bool Active = true;
+}
--- Content.Shared/SimpleStation14/Drone/HologramComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared.SimpleStation14.Hologram
-{
-    [RegisterComponent]
-    public sealed class HologramComponent : Component
-    {
-        [ViewVariables]
-        public EntityUid? CurProjector;
-    }
-}
