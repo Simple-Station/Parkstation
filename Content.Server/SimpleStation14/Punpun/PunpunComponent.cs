b4352c48f2fe0d8cbab694673e14a7db62e4ab98
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-03-08 15:37:35 -0500

    Remove some unneeded filler

--- Content.Server/SimpleStation14/Punpun/PunpunComponent.cs
+++ Content.Server/SimpleStation14/Punpun/PunpunComponent.cs
@@ -1,9 +1,8 @@
-// new component named punpuncomponent
 namespace Content.Server.SimpleStation14.Punpun;
 
 [RegisterComponent]
 public class PunpunComponent : Component
 {
-    // EntityUid to store the owner of the component
+    // EntityUid to store the Punpun in question
     public EntityUid owner = EntityUid.Invalid;
 }
