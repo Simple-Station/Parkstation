d9cbba8fc3287edad69f6fa083963e4cff97ed47
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-03-14 22:35:11 -0700

    Buff passive heal

--- Content.Shared/SimpleStation14/Magic/Asclepius/Components/HippocraticOathComponent.cs
+++ Content.Shared/SimpleStation14/Magic/Asclepius/Components/HippocraticOathComponent.cs
@@ -16,20 +16,19 @@ namespace Content.Shared.SimpleStation14.Magic.Asclepius.Components
         [ViewVariables(VVAccess.ReadWrite)]
         public Dictionary<string, float> DamageTypes = new()
         {
-            // Overall 1.075
-            // Brute (0.45)
-            { "Blunt", 0.20f },
-            { "Slash", 0.125f },
-            { "Piercing", 0.125f },
-            // Burn (0.4)
-            { "Heat", 0.16f },
-            { "Cold", 0.16f },
-            { "Shock", 0.08f },
-            // Airloss (0.2)
-            { "Airloss", 0.15f },
-            { "Bloodloss", 0.05f },
-            // Genetic (0.025)
-            { "Genetic", 0.025f },
+            // Brute
+            { "Blunt", 0.25f },
+            { "Slash", 0.2f },
+            { "Piercing", 0.15f },
+            // Burn
+            { "Heat", 0.225f },
+            { "Cold", 0.2f },
+            { "Shock", 0.1f },
+            // Airloss
+            { "Airloss", 0.25f },
+            { "Bloodloss", 0.1f },
+            // Genetic
+            { "Genetic", 0.05f },
         };
     }
 }
