b6c92cb1ae59c6e2eeb2e27c58b8b224a54af7f9
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-03-22 22:45:00 -0700

    ccvars

--- Content.Shared/SimpleStation14/CCVar/CCVars.cs
+++ Content.Shared/SimpleStation14/CCVar/CCVars.cs
@@ -5,9 +5,50 @@ namespace Content.Shared.SimpleStation14.CCVar;
 [CVarDefs]
 public sealed class SimpleStationCVars
 {
+    public static readonly List<string> CargoShuttleCostMethods = new() {"flat", "timed", "percent", "load", "none"};
+
     /// <summary>
-    /// How much money to remove from cargo when the shuttle FTLs
+    ///     The method used to calculate the cost of the cargo shuttle.
     /// </summary>
-    public static readonly CVarDef<int> CargoShuttleCost =
-        CVarDef.Create("cargo.shuttle.cost", 100, CVar.SERVER);
+    /// <remarks>
+    ///     Valid values are stored in <see cref="CargoShuttleCostMethods"/>.
+    /// </remarks>
+    public static readonly CVarDef<string> CargoCostMethod =
+        CVarDef.Create("cargo.shuttle.method", "flat", CVar.SERVER);
+
+    /// <summary>
+    ///     Allow the cost to increase with debt but still call the shuttle if you can't pay it.
+    /// </summary>
+    public static readonly CVarDef<bool> CargoDebt =
+        CVarDef.Create("cargo.debt", false, CVar.SERVER);
+
+    /// <summary>
+    ///     The minimum/starting amount for the cargo shuttle to use when flying.
+    /// </summary>
+    public static readonly CVarDef<int> CargoShuttleBaseCost =
+        CVarDef.Create("cargo.shuttle.basecost", 100, CVar.SERVER);
+
+    /// <summary>
+    ///     The maximum amount of money the cargo shuttle will cost.
+    /// </summary>
+    public static readonly CVarDef<int> CargoShuttleMaxCost =
+        CVarDef.Create("cargo.shuttle.maxcost", 1000, CVar.SERVER);
+
+    /// <summary>
+    ///     The amount of money the cargo shuttle will cost per minute.
+    /// </summary>
+    public static readonly CVarDef<int> CargoShuttleTimedCost =
+        CVarDef.Create("cargo.shuttle.timedcost", 5, CVar.SERVER);
+
+    /// <summary>
+    ///     The percent of money to take when flying the cargo shuttle.
+    /// </summary>
+    public static readonly CVarDef<int> CargoShuttlePercentCost =
+        CVarDef.Create("cargo.shuttle.percentcost", 10, CVar.SERVER);
+
+    /// <summary>
+    ///     The amount of money the cargo shuttle will cost per order.
+    /// </summary>
+    public static readonly CVarDef<int> CargoShuttleLoadCost =
+        CVarDef.Create("cargo.shuttle.loadcost", 75, CVar.SERVER);
 }
