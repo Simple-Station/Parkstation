0d06e8ed9a98eb301fcbc7bdbdea19aee588f2a2
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-03-28 15:51:17 -0700

    test 15

--- Tools/changelog/changelog.js
+++ Tools/changelog/changelog.js
@@ -2,20 +2,15 @@ const fs = require("fs");
 const yaml = require("js-yaml");
 const axios = require("axios");
 
-// Examples of how the variables will output
-// process.env.PR_NUMBER = "29";
-// process.env.GITHUB_TOKEN = "github_pat_tokenTHINGgoesHERE111";
-// process.env.GITHUB_REPOSITORY = "Park-Station/ParkStation";
-// process.env.CHANGELOG_DIR = "../../Resources/Changelog/SimpleStationChangelog.yml";
-
 // Using a token is optional, but will greatly reduce the chance of hitting the rate limit
 if (process.env.GITHUB_TOKEN) axios.defaults.headers.common["Authorization"] = `Bearer ${process.env.GITHUB_TOKEN}`;
 
 (async () => {
-    console.log(process.env.PR_NUMBER);
-    console.log(process.env.GITHUB_TOKEN != undefined && process.env.GITHUB_TOKEN != "");
-    console.log(process.env.GITHUB_REPOSITORY);
-    console.log(process.env.CHANGELOG_DIR);
+    // Debug
+    console.log(`PR Number: ${process.env.PR_NUMBER}`);
+    console.log(`Using Token: ${process.env.GITHUB_TOKEN != undefined && process.env.GITHUB_TOKEN != ""}`);
+    console.log(`Repository: ${process.env.GITHUB_REPOSITORY}`);
+    console.log(`Changelog Directory: ${process.env.CHANGELOG_DIR}`);
 
     // Get PR details
     const pr = await axios.get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/pulls/${process.env.PR_NUMBER}`);
