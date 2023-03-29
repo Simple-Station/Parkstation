a7488ae3a714b2a808d701745b5abaa96f471377
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-03-29 17:38:54 -0400

    Cleaned up code, fixed some bugs, added Beenade sprite

--- Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerComponent.cs
+++ Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerComponent.cs
@@ -1,20 +1,57 @@
 namespace Content.Server.SimpleStation14.Grenades;
 
 [RegisterComponent]
+
+/// <summary>
+///     Spawns entities when triggered.
+///     This is used for grenades and other things that spawn entities on trigger.
+/// </summary>
 public sealed class SpawnEntitiesOnTriggerComponent : Component
 {
+    /// <summary>
+    ///     The prototype of the entity to spawn.
+    /// </summary>
     [DataField("prototype")]
     public string? Prototype = null;
 
+    /// <summary>
+    ///     The amount of entities to spawn.
+    /// </summary>
     [DataField("count")]
     public int Count = 1;
 
+    /// <summary>
+    ///     The minimum amount of entities to spawn.
+    ///     If this is null, will always spawn full amount.
+    /// </summary>
     [DataField("minCount")]
     public int? MinCount = null;
 
+    /// <summary>
+    ///     The velocity to either shoot or throw the spawned entities with.
+    ///     If this is null, entities won't be thrown or shot.
+    /// </summary>
     [DataField("velocity")]
     public float? Velocity = null;
 
+    /// <summary>
+    ///     The time in seconds before the spawned entities despawn.
+    ///     If this is null, entities won't despawn.
+    /// </summary>
     [DataField("despawnTime")]
     public float? DespawnTime = null;
+
+    /// <summary>
+    ///     Whether or to shoot the spawned entities.
+    ///     If this is false, entities with velocity will be thrown.
+    /// </summary>
+    [DataField("shoot")]
+    public bool Shoot = false;
+
+    /// <summary>
+    ///     The offset multiplier for the spawned entities in max tiles.
+    ///     If this is 0, entities will spawn on top of the trigger.
+    /// </summary>
+    [DataField("offset")]
+    public float Offset = 0f;
 }
--- Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerSystem.cs
+++ Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerSystem.cs
@@ -13,6 +13,7 @@ using Robust.Shared.Physics;
 using Robust.Shared.Physics.Systems;
 using Content.Server.Weapons.Ranged.Systems;
 using Content.Shared.Weapons.Ranged.Systems;
+using Content.Shared.Throwing;
 
 namespace Content.Server.SimpleStation14.Grenades;
 
@@ -23,6 +24,7 @@ public class SpawnEntitiesOnTriggerSystem : EntitySystem
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedPhysicsSystem _physicsSystem = default!;
     [Dependency] private readonly GunSystem _gunSystem = default!;
+    [Dependency] private readonly ThrowingSystem _throwingSystem = default!;
 
     public override void Initialize()
     {
@@ -31,6 +33,11 @@ public class SpawnEntitiesOnTriggerSystem : EntitySystem
         SubscribeLocalEvent<SpawnEntitiesOnTriggerComponent, TriggerEvent>(OnTrigger);
     }
 
+    /// <summary>
+    ///     On trigger, works out if the entity has a variable count, decides how many to spawn,
+    ///     then iterates through and spawns each one apply any offset, and any velocity, be it thrown or shot,
+    ///     spinning them in a random direction. Finally, sets the entity to be deleted if a despawn time is set.
+    /// </summary>
     private void OnTrigger(EntityUid uid, SpawnEntitiesOnTriggerComponent component, TriggerEvent args)
     {
         if (component.Prototype == null) return;
@@ -46,18 +53,26 @@ public class SpawnEntitiesOnTriggerSystem : EntitySystem
 
         for (var i = 0; i < spawnCount; i++)
         {
-            var spawnedEntity = _entityManager.SpawnEntity(prototype.ID, _entityManager.GetComponent<TransformComponent>(uid).Coordinates);
+            var spawnedEntity = _entityManager.SpawnEntity(prototype.ID, _entityManager.GetComponent<TransformComponent>(uid).Coordinates
+                + new EntityCoordinates(_entityManager.GetComponent<TransformComponent>(uid).ParentUid, _random.NextVector2(-1, 1) * component.Offset));
+
+            var transfComp = _entityManager.GetComponent<TransformComponent>(spawnedEntity);
+            transfComp.AttachToGridOrMap();
 
             if (component.Velocity != null)
             {
-                var transfComp = _entityManager.GetComponent<TransformComponent>(spawnedEntity);
                 var physicsComp = _entityManager.GetComponent<PhysicsComponent>(spawnedEntity);
 
                 transfComp.LocalRotation = Angle.FromDegrees(_random.Next(0, 360));
 
-                _gunSystem.ShootProjectile(spawnedEntity, transfComp.LocalRotation.ToWorldVec(), Vector2.Zero, speed: component.Velocity.Value * _random.NextFloat(0.8f, 1.2f));
-
-                // _physicsSystem.SetLinearVelocity(spawnedEntity, transfComp.LocalRotation.ToWorldVec() * component.Velocity.Value * _random.NextFloat(0.8f, 1.2f));
+                if (component.Shoot)
+                {
+                    _gunSystem.ShootProjectile(spawnedEntity, transfComp.LocalRotation.ToWorldVec(), Vector2.Zero, speed: component.Velocity.Value * _random.NextFloat(0.8f, 1.2f));
+                }
+                else
+                {
+                    _throwingSystem.TryThrow(spawnedEntity, transfComp.LocalRotation.ToWorldVec(), 1.0f, null, 5.0f, physicsComp, transfComp);
+                }
             }
 
             if (component.DespawnTime != null)
