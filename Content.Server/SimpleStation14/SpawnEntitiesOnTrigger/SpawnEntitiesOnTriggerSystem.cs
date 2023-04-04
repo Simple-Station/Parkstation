88164058a2c2eecc1e5cdb15ed7a487d5b0be009
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-04-04 10:38:22 -0400

    Trying out async methods for performance, maybe keep?

--- Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerSystem.cs
+++ Content.Server/SimpleStation14/SpawnEntitiesOnTrigger/SpawnEntitiesOnTriggerSystem.cs
@@ -1,26 +1,21 @@
-using System.Collections.Generic;
 using Content.Shared.Spawners.Components;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Content.Server.Explosion.EntitySystems;
-using Content.Server.Damage.Components;
-using Content.Shared.Damage;
 using Robust.Shared.Random;
 using Robust.Shared.Physics.Components;
-using Robust.Shared.Physics;
 using Robust.Shared.Physics.Systems;
 using Content.Server.Weapons.Ranged.Systems;
-using Content.Shared.Weapons.Ranged.Systems;
 using Content.Shared.Throwing;
+using System.Threading.Tasks;
+using Robust.Shared.Utility;
+using System.Collections.Generic;
 
 namespace Content.Server.SimpleStation14.Grenades;
 
 public class SpawnEntitiesOnTriggerSystem : EntitySystem
 {
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedPhysicsSystem _physicsSystem = default!;
     [Dependency] private readonly GunSystem _gunSystem = default!;
@@ -38,11 +33,16 @@ public class SpawnEntitiesOnTriggerSystem : EntitySystem
     ///     then iterates through and spawns each one apply any offset, and any velocity, be it thrown or shot,
     ///     spinning them in a random direction. Finally, sets the entity to be deleted if a despawn time is set.
     /// </summary>
-    private void OnTrigger(EntityUid uid, SpawnEntitiesOnTriggerComponent component, TriggerEvent args)
+    private async void OnTrigger(EntityUid uid, SpawnEntitiesOnTriggerComponent component, TriggerEvent args)
     {
         if (component.Prototype == null) return;
 
-        var prototype = _prototypeManager.Index<EntityPrototype>(component.Prototype);
+        if (!_prototypeManager.TryIndex<EntityPrototype>(component.Prototype, out var prototype))
+        {
+            DebugTools.Assert($"Prototype {component.Prototype} does not exist");
+
+            return;
+        }
 
         var spawnCount = component.Count;
 
@@ -51,35 +51,55 @@ public class SpawnEntitiesOnTriggerSystem : EntitySystem
             spawnCount = _random.Next(component.MinCount.Value, component.Count);
         }
 
-        for (var i = 0; i < spawnCount; i++)
+        var spawnedEntities = await SpawnEntities(spawnCount, prototype, component, uid, Transform(uid));
+
+        foreach (var spawnedEntity in spawnedEntities)
         {
-            var spawnedEntity = _entityManager.SpawnEntity(prototype.ID, _entityManager.GetComponent<TransformComponent>(uid).Coordinates
-                + new EntityCoordinates(_entityManager.GetComponent<TransformComponent>(uid).ParentUid, _random.NextVector2(-1, 1) * component.Offset));
-
-            var transfComp = _entityManager.GetComponent<TransformComponent>(spawnedEntity);
-            transfComp.AttachToGridOrMap();
-
-            if (component.Velocity != null)
-            {
-                var physicsComp = _entityManager.GetComponent<PhysicsComponent>(spawnedEntity);
-
-                transfComp.LocalRotation = Angle.FromDegrees(_random.Next(0, 360));
-
-                if (component.Shoot)
-                {
-                    _gunSystem.ShootProjectile(spawnedEntity, transfComp.LocalRotation.ToWorldVec(), Vector2.Zero, speed: component.Velocity.Value * _random.NextFloat(0.8f, 1.2f));
-                }
-                else
-                {
-                    _throwingSystem.TryThrow(spawnedEntity, transfComp.LocalRotation.ToWorldVec(), 1.0f, null, 5.0f, physicsComp, transfComp);
-                }
-            }
-
             if (component.DespawnTime != null)
             {
-                var despawnComp = _entityManager.EnsureComponent<TimedDespawnComponent>(spawnedEntity);
+                var despawnComp = EntityManager.EnsureComponent<TimedDespawnComponent>(spawnedEntity);
                 despawnComp.Lifetime = component.DespawnTime.Value;
             }
         }
+    }
+
+    /// <summary>
+    ///     Async method to spawn the entities to prevent lag spikes.
+    /// </summary>
+    private async Task<List<EntityUid>> SpawnEntities(int spawnCount, EntityPrototype prototype, SpawnEntitiesOnTriggerComponent component, EntityUid uid, TransformComponent transfComp)
+    {
+        var spawnedEntities = new List<EntityUid>();
+
+        for (var i = 0; i < spawnCount * 100000; i++)
+        {
+            if (i % 100000 != 0) continue;
+
+            var spawnedEntity = EntityManager.SpawnEntity(prototype.ID, transfComp.Coordinates
+                + new EntityCoordinates(transfComp.ParentUid, _random.NextVector2(-1, 1) * component.Offset));
+
+            Transform(spawnedEntity).AttachToGridOrMap();
+
+            // If the component has a velocity set, prepare to launch the shrapnel.
+            if (component.Velocity != null)
+            {
+                var physicsComp = EntityManager.GetComponent<PhysicsComponent>(spawnedEntity);
+
+                Transform(spawnedEntity).LocalRotation = Angle.FromDegrees(_random.Next(0, 360));
+
+                // Decide to either shoot, or throw.
+                if (component.Shoot)
+                {
+                    _gunSystem.ShootProjectile(spawnedEntity, Transform(spawnedEntity).LocalRotation.ToWorldVec(), Vector2.Zero, speed: component.Velocity.Value * _random.NextFloat(0.8f, 1.2f));
+                }
+                else
+                {
+                    _throwingSystem.TryThrow(spawnedEntity, Transform(spawnedEntity).LocalRotation.ToWorldVec(), 1.0f, null, 5.0f, physicsComp, Transform(spawnedEntity));
+                }
+            }
+
+            spawnedEntities.Add(spawnedEntity);
+        }
+
+        return spawnedEntities;
     }
 }
