43d4de49d731efb6041c89567ca53004f6ed6f36
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-04-30 00:13:50 -0700

    Improve station goals system

--- Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
+++ Content.Server/Corvax/StationGoal/StationGoalPaperSystem.cs
@@ -1,6 +1,8 @@
+using System.Data;
 using System.Linq;
 using Content.Server.Fax;
 using Content.Shared.GameTicking;
+using Content.Shared.Random;
 using Content.Shared.Random.Helpers;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
@@ -16,9 +18,12 @@ namespace Content.Server.Corvax.StationGoal
         [Dependency] private readonly IRobustRandom _random = default!;
         [Dependency] private readonly FaxSystem _faxSystem = default!;
 
+        private const string RandomPrototype = "StationGoals";
+
         public override void Initialize()
         {
             base.Initialize();
+
             SubscribeLocalEvent<RoundStartedEvent>(OnRoundStarted);
         }
 
@@ -27,31 +32,60 @@ namespace Content.Server.Corvax.StationGoal
             SendRandomGoal();
         }
 
+        /// <summary>
+        ///     Send a random station goal to all faxes which are authorized to receive it
+        /// </summary>
+        /// <returns>If the fax was successful</returns>
+        /// <exception cref="ConstraintException">Raised when station goal types in the prototype is invalid</exception>
         public bool SendRandomGoal()
         {
-            var availableGoals = _prototypeManager.EnumeratePrototypes<StationGoalPrototype>().ToList();
-            var goal = _random.Pick(availableGoals);
+            // Get the random station goal list
+            if (!_prototypeManager.TryIndex<WeightedRandomPrototype>(RandomPrototype, out var goals))
+                return false;
+
+            // Get a random goal
+            var goal = RecursiveRandom(goals);
+
+            // Send the goal
             return SendStationGoal(goal);
         }
 
+        private StationGoalPrototype RecursiveRandom(WeightedRandomPrototype random)
+        {
+            var goal = random.Pick(_random);
+
+            if (_prototypeManager.TryIndex<StationGoalPrototype>(goal, out var goalPrototype))
+            {
+                return goalPrototype;
+            }
+
+            if (_prototypeManager.TryIndex<WeightedRandomPrototype>(goal, out var goalRandom))
+            {
+                return RecursiveRandom(goalRandom);
+            }
+
+            throw new Exception($"StationGoalPaperSystem: Random station goal could not be found from origin prototype {RandomPrototype}");
+        }
+
         /// <summary>
-        ///     Send a station goal to all faxes which are authorized to receive it.
+        ///     Send a station goal to all faxes which are authorized to receive it
         /// </summary>
         /// <returns>True if at least one fax received paper</returns>
         public bool SendStationGoal(StationGoalPrototype goal)
         {
             var faxes = EntityManager.EntityQuery<FaxMachineComponent>();
             var wasSent = false;
-            foreach (var fax in faxes)
-            {
-                if (!fax.ReceiveStationGoal) continue;
 
+            foreach (var fax in faxes.Where(f => f.ReceiveStationGoal))
+            {
                 var printout = new FaxPrintout(
                     Loc.GetString(goal.Text),
                     Loc.GetString("station-goal-fax-paper-name"),
                     null,
                     "paper_stamp-cent",
-                    new() { Loc.GetString("stamp-component-stamped-name-centcom") });
+                    new List<string> { Loc.GetString("stamp-component-stamped-name-centcom") }
+                );
+
                 _faxSystem.Receive(fax.Owner, printout, null, fax);
 
                 wasSent = true;
