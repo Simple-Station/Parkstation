0e87968b2d81c978781ca0723f63d3c6a5bafb14
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-05-03 21:08:35 -0700

    add feared component to most matching things

--- Content.Shared/SimpleStation14/Traits/SightFear/SightFearTraitComponent.cs
+++ Content.Shared/SimpleStation14/Traits/SightFear/SightFearTraitComponent.cs
@@ -3,6 +3,7 @@ namespace Content.Shared.SimpleStation14.Traits.SightFear
     [RegisterComponent, AutoGenerateComponentState]
     public sealed partial class SightFearTraitComponent : Component
     {
+        // TODO: Check if the ID is valid
         [DataField("afraidOf"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
         public string AfraidOf = string.Empty;
     }
--- Content.Shared/SimpleStation14/Traits/SightFear/SightFearedComponent.cs
+++ Content.Shared/SimpleStation14/Traits/SightFear/SightFearedComponent.cs
@@ -3,6 +3,7 @@ namespace Content.Shared.SimpleStation14.Traits.SightFear
     [RegisterComponent, AutoGenerateComponentState]
     public sealed partial class SightFearedComponent : Component
     {
+        // TODO: Check if the IDs are valid
         [DataField("fears"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
         public Dictionary<string, float> Fears = new();
     }
