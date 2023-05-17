1dec5cb58023a96f894e7b68eb0e71930d8173fa
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-05-16 22:03:14 -0700

    remove ignored prototypes on save test

--- Content.IntegrationTests/Tests/PrototypeSaveTest.cs
+++ Content.IntegrationTests/Tests/PrototypeSaveTest.cs
@@ -37,15 +37,6 @@ public sealed class PrototypeSaveTest
         "Singularity", // physics collision uses "AllMask" (-1). The flag serializer currently fails to save this because this features un-named bits.
         "constructionghost",
         "MobGlimmerWisp", // fucking combat mode bullshit
-        // TODO: I don't want to figure out what's wrong with these right now
-        "ClothingOuterPurpleCoat",
-        "MagicMedipen",
-        "Sleeper",
-        "SleeperPod",
-        "SleeperParty",
-        "SleeperClockwork",
-        "SleeperSyndicate",
-        "WeaponFlamethrower",
     };
 
     [Test]
