be28f08ffaa11934dfeac087e07c0b510370d9d5
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-07-23 01:21:59 -0400

    Minor fixes post-merge

--- Content.Server/SimpleStation14/Power/Components/RandomBatteryChargeComponent.cs
+++ Content.Server/SimpleStation14/Power/Components/RandomBatteryChargeComponent.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Content.Server.SimpleStation14.Power.Components;
 
 [RegisterComponent]
@@ -7,13 +9,13 @@ public sealed class RandomBatteryChargeComponent : Component
     ///     The minimum and maximum max charge the battery can have.
     /// </summary>
     [DataField("batteryMaxMinMax")]
-    public Vector2 BatteryMaxMinMax = (0.85f, 1.15f);
+    public Vector2 BatteryMaxMinMax = new(0.85f, 1.15f);
 
     /// <summary>
     ///     The minimum and maximum current charge the battery can have.
     /// </summary>
     [DataField("batteryChargeMinMax")]
-    public Vector2 BatteryChargeMinMax = (1f, 1f);
+    public Vector2 BatteryChargeMinMax = new(1f, 1f);
 
     /// <summary>
     ///     True if the current charge is based on the preexisting current charge, or false if it's based on the max charge.
