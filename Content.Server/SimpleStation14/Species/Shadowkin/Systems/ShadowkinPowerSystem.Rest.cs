6f88662ff076df9aff1b8bad8aaf12394460fd4b
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-09-17 17:36:50 -0700

    lower base power gain

--- Content.Server/SimpleStation14/Species/Shadowkin/Systems/ShadowkinPowerSystem.Rest.cs
+++ Content.Server/SimpleStation14/Species/Shadowkin/Systems/ShadowkinPowerSystem.Rest.cs
@@ -59,7 +59,7 @@ public sealed class ShadowkinRestSystem : EntitySystem
             _entity.EnsureComponent<ForcedSleepingComponent>(args.Performer);
             // No waking up normally (it would do nothing)
             _actions.RemoveAction(args.Performer, new InstantAction(_prototype.Index<InstantActionPrototype>("Wake")));
-            _power.TryAddMultiplier(args.Performer, 1.5f);
+            _power.TryAddMultiplier(args.Performer, 2f);
             // No action cooldown
             args.Handled = false;
         }
@@ -69,7 +69,7 @@ public sealed class ShadowkinRestSystem : EntitySystem
             // Wake up
             _entity.RemoveComponent<ForcedSleepingComponent>(args.Performer);
             _entity.RemoveComponent<SleepingComponent>(args.Performer);
-            _power.TryAddMultiplier(args.Performer, -1.5f);
+            _power.TryAddMultiplier(args.Performer, -2f);
             // Action cooldown
             args.Handled = true;
         }
