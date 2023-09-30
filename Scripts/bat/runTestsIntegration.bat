55f29073eaba341f7bb63354e74210d11e5f8d26
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-09-30 15:56:00 -0700

    YAML linter script

--- Scripts/bat/runTestsIntegration.bat
+++ Scripts/bat/runTestsIntegration.bat
@@ -5,5 +5,5 @@ mkdir Scripts\logs
 del Scripts\logs\Content.Tests.log
 dotnet test --no-build --configuration DebugOpt Content.Tests/Content.Tests.csproj -- NUnit.ConsoleOut=0 > Scripts\logs\Content.Tests.log
 del Scripts\logs\Content.IntegrationTests.log
-dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj -- NUnit.ConsoleOut=0 > Scripts\logs\Content.IntegrationTests.log
+dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj -- NUnit.ConsoleOut=0 NUnit.MapWarningTo=Failed > Scripts\logs\Content.IntegrationTests.log
 pause
