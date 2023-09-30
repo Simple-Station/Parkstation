3516cb986ae7c6cff4998d17285c1ca4f097d231
DEATHB4DEFEAT <zachcaffee@outlook.com>
2023-09-30 15:17:00 -0700

    improve script

--- Scripts/bat/runIntegrationTests.bat
+++ Scripts/bat/runIntegrationTests.bat
@@ -1,7 +1,9 @@
-@echo off
-cd ../../
+cd ..\..\
 dotnet restore
 dotnet build --configuration DebugOpt --no-restore /p:WarningsAsErrors=nullable /m
-dotnet test --no-build --configuration DebugOpt Content.Tests/Content.Tests.csproj -- NUnit.ConsoleOut=0
-dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj -- NUnit.ConsoleOut=0
+mkdir Scripts\logs
+del Scripts\logs\Content.Tests.log
+dotnet test --no-build --configuration DebugOpt Content.Tests/Content.Tests.csproj -- NUnit.ConsoleOut=0 > Scripts\logs\Content.Tests.log
+del Scripts\logs\Content.IntegrationTests.log
+dotnet test --no-build --configuration DebugOpt Content.IntegrationTests/Content.IntegrationTests.csproj -- NUnit.ConsoleOut=0 > Scripts\logs\Content.IntegrationTests.log
 pause
